86e8282e29c184efd5d628dc7e4df79b941f6b51
David Castillo <dc4stillo89@gmail.com>
2016-02-08 11:07:15 +0100

    Added CallableStatement method and conductor

--- src/ejemplos/ConductorWPFSample.java
+++ src/ejemplos/ConductorWPFSample.java
@@ -1,5 +1,26 @@
 package ejemplos;
 
-public class ConductorWPFSample {
+import java.sql.Date;
 
+public class ConductorWPFSample {
+	public static void main(String[] args) {
+		ManejadoraWPFSample maneja = new ManejadoraWPFSample();
+		int idPersonaPrueba;
+		
+		//Datos de la persona a insertar
+		String nombre, apellidos, direccion, telefono;
+		Date fecha_nacimiento;
+		
+		nombre = "Jesus";
+		apellidos = "García";
+		fecha_nacimiento = new Date(System.currentTimeMillis());
+		direccion = "Calle Trolazo 4";
+		telefono = "777555333";
+		
+		//Llamo a la función para insertar y muestro el resultado por pantalla
+		idPersonaPrueba = maneja.insertarPersonaSP(nombre, apellidos, fecha_nacimiento, direccion, telefono);
+		
+		System.out.println("La id de la persona insertada es: " + idPersonaPrueba);
+	}
+	
 }
--- src/ejemplos/ManejadoraWPFSample.java
+++ src/ejemplos/ManejadoraWPFSample.java
@@ -1,6 +1,8 @@
 package ejemplos;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -9,6 +11,14 @@ import java.sql.Statement;
 
 public class ManejadoraWPFSample {
 	
+	/* void insertPersona(String nombre, String apellidos)
+	 * 
+	 * Comentario: Método para insertar una persona en la base de datos dado su nombre y su apellido
+	 * Precondición: Nada
+	 * Entrada: Dos cadenas (nombre y apellido)
+	 * Salida: Nada
+	 * Postcondición: La persona queda insertada 
+	 */
 	public void insertPersona(String nombre, String apellidos) {
 		final String USE = "Use [WPFSample]";
 		Connection connexionBaseDatos = null;
@@ -46,6 +56,57 @@ public class ManejadoraWPFSample {
 				e.printStackTrace();
 			}
 		}
+	}
+	
+	/* int insertarPersonaSP(String nombre, String apellidos, String fecha_nacimiento, String Direccion, String Telefono)
+	 * 
+	 * Comentario: Método para insertar una persona en la base de datos usando CallableStatement
+	 * Precondición: Nada
+	 * Entrada: Cinco String que representan los datos de la persona
+	 * Salida: Un entero
+	 * Postcondición: Se devuelve un entero asociado al nombre. Representa el id de la persona insertada.
+	 */
+	public int insertarPersonaSP(String nombre, String apellidos, Date fecha_nacimiento, String Direccion, String Telefono) {
+		int idPersonaInsertada = 0;
+		final String USE = "Use [WPFSample]";
+		Connection connexionBaseDatos = null;		
+		
+		try {
+			//Cargo la clase del driver
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");			
+			
+			//Asigno los valores de la conexión y establezco la conexión
+			String sourceURL = "jdbc:sqlserver://localhost";
+		    String usuario = "prueba";
+		    String password = "123";
+		    connexionBaseDatos = DriverManager.getConnection(sourceURL, usuario, password);
+		    
+		    //Creo la sentencia USE para establecer la base de datos
+		    Statement sentenciaUSE = connexionBaseDatos.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);		    
+		    sentenciaUSE.execute(USE);
+		    
+		    //Creo la sentencia de inserción		    
+			String sentencia = "EXECUTE sp_insertarPersona ?,?,?,?,?,?";		
+			CallableStatement callableStatement = connexionBaseDatos.prepareCall(sentencia);
+			
+			//Asigno valores a los parámetros
+			callableStatement.setString(1, nombre);
+			callableStatement.setString(2, apellidos);
+			callableStatement.setDate(3, fecha_nacimiento);
+			callableStatement.setString(4, Direccion);
+			callableStatement.setString(5, Telefono);
+			callableStatement.registerOutParameter(6, java.sql.Types.INTEGER);
+			
+			//Ejecuto la sentencia y recojo el valor devuelto
+			callableStatement.executeUpdate();
+			idPersonaInsertada = callableStatement.getInt(6);			
+		} catch (SQLException e) {
+			System.out.println("Fallo de conexión con el servidor" + e.getMessage());
+		} catch (ClassNotFoundException e) {
+			System.out.println("No se encuentra la clase del driver SQL");
+		}
+		
+		return idPersonaInsertada;
 	}
 	
 }
